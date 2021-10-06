eb52cd0f91d55801282fdcb6243e0eed7b535dd7
WON-JIN-LEE <vopgh0409@gmail.com>
2021-10-06 15:13:46 +0900

    Publish useTitle to Npm

--- src/App.js
+++ src/App.js
@@ -1,64 +1,14 @@
-import React, {useState, useEffect, useRef} from 'react';
-
-const useFullScreen = (callback) => {
-    const element = useRef();
-    const runCb = isFull => {
-        if (callback && typeof callback === "function") {
-            callback(isFull);
-        }
-    };
-    const triggerFull = () => {
-        if (element.current) {
-            if (element.current.requestFullscreen) {
-                element.current.requestFullscreen();
-            } else if (element.current.mozRequestFullScreen) {
-                element.current.mozRequestFullScreen();
-            } else if (element.current.webkitRequestFullscreen) {
-                element.current.webkitRequestFullscreen();
-            } else if (element.current.msRequestFullscreen) {
-                element.current.msRequestFullscreen();
-            }
-            runCb(true);
-        }
-    };
-
-    const exitFull = () => {
-        document.exitFullscreen();
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.mozCancelFullScreen) {
-            document.mozCancelFullScreen();
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-        }
-        runCb(false);
-    };
-    return { element, triggerFull, exitFull };
-    
-};
-
+import useAxios from "./useAxios"
 
 const App = () => {
-    const onFullS = (isFull) => {
-        console.log(
-            isFull
-                ? "We are Full"
-                : "We are small"
-        );
-    }
-    const {element, triggerFull, exitFull} = useFullScreen(onFullS);
-    return (
-        <div className="App" style={{
-                height: "1000vh"
-            }}>
-            <div ref={element}>
-                <img src="https://won-jin-lee.github.io/portfolio-Web/imgs/me.jpg"/>
-                <button onClick={exitFull}>Exit FullScreen</button>
+        const {loading, data, error,refetch} = useAxios({url:"https://yts.mx/api/v2/list_movies.json"});
 
-            </div>
-            <button onClick={triggerFull}>Make FullScreen</button>
+    console.log(loading, data, error);
+    return (
+        <div className="App">
+            <h1>{data && data.status}</h1>
+            <h2>{loading && "Loading"}</h2>
+            <button onClick={refetch}>refetch</button>
         </div>
     );
 };
